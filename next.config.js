58c91cb8fbf613a8268cd0860ab51ed85b5b9c98
gitaman8481 <gitaman8481@gmail.com>
2021-11-08 19:20:54 +0530

    next application created

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    image: {
+        domains: ["links.papareact.com"]
+    }
+}
---
+++ pages/data.js
@@ -0,0 +1,33 @@
+export const products = [
+  {
+    id: 1,
+    img: "../img/movie.webp",
+    link: "http://lama.dev",
+    vide0: "./img/ecommerce.gif"
+  },
+  {
+    id: 2,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "http://lama.dev",
+  },
+];
