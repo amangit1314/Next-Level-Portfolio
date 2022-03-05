e91b52f2800416253c5e81f6de39d8bdb08dca09
amangit1314 <gitaman8481@gmail.com>
2022-03-05 15:41:33 +0530

    Removed deployment errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
