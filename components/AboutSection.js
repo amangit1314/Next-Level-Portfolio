e951997a5de90728b198e255a5a19f4857439b92
amangit1314 <gitaman8481@gmail.com>
2023-01-01 17:10:53 +0530

    addressing deployment issues

--- components/AboutSection.js
+++ components/AboutSection.js
@@ -5,17 +5,17 @@ import { SiWeb3Dotjs } from "react-icons/si";
 
 function AboutSection() {
   return (
-    <div className="items-center flex flex-col align-middle mx-auto p-8 max-w-7xl justify-between">
+    <div className="flex flex-col items-center justify-between p-8 mx-auto align-middle max-w-7xl">
       <p className="mb-24 text-4xl font-semibold text-center about-txt">
         About Me
       </p>
       <section
         id="aboutSect"
-        className="flex items-center justify-center mb-24 about"
+        className="flex items-center justify-between mb-24 about max-w-7xl"
       >
-        <div className="align-middle items-center">
+        <div className="items-center align-middle">
           <Image
-            className="object-cover cursor-pointer overflow-hidden bg-gray-900 rounded-lg shadow-lg about-left left-img"
+            className="object-cover overflow-hidden bg-gray-900 rounded-lg shadow-lg cursor-pointer about-left left-img"
             src="/images/jpg/ar.jpg"
             alt="Loading..."
             height={300}
@@ -24,9 +24,9 @@ function AboutSection() {
           />
         </div>
 
-        <div className="felx flex-col flex-1 w-6/12 text-white about-right">
+        <div className="flex-col flex-1 w-6/12 text-white felx about-right">
           <div className="flex w-7/12 h-auto mb-10 ml-24 rounded-lg hover:shadow-md">
-            <div className="h-40 p-2 m-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200 text-gray-100 bg-gray-600 rounded-lg cursor-pointer hover:bg-purple-500 hover:text-white w-65">
+            <div className="h-40 p-2 m-2 text-gray-100 transition duration-200 ease-in-out delay-150 bg-gray-600 rounded-lg cursor-pointer hover:-translate-y-1 hover:scale-110 hover:bg-purple-500 hover:text-white w-65">
               <button className="items-center mt-2 ml-3">
                 {" "}
                 <HiOutlineDeviceTablet size={42} />{" "}
@@ -36,7 +36,7 @@ function AboutSection() {
                 2+ Years of expr building projects with flutter
               </div>
             </div>
-            <div className="h-40 p-2 m-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200 text-gray-100 bg-gray-600 rounded-lg cursor-pointer hover:shadow-md hover:bg-purple-500 hover:text-white w-65">
+            <div className="h-40 p-2 m-2 text-gray-100 transition duration-200 ease-in-out delay-150 bg-gray-600 rounded-lg cursor-pointer hover:-translate-y-1 hover:scale-110 hover:shadow-md hover:bg-purple-500 hover:text-white w-65">
               <button className="items-center mt-2 ml-3">
                 {" "}
                 <SiWeb3Dotjs size={42} />{" "}
@@ -46,7 +46,7 @@ function AboutSection() {
                 I like to build stuff on web3 and full stack with Next.js
               </div>
             </div>
-            <div className="h-40 p-2 m-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-200 text-gray-100 bg-gray-600 rounded-lg cursor-pointer hover:shadow-md hover:bg-purple-500 hover:text-white w-65">
+            <div className="h-40 p-2 m-2 text-gray-100 transition duration-200 ease-in-out delay-150 bg-gray-600 rounded-lg cursor-pointer hover:-translate-y-1 hover:scale-110 hover:shadow-md hover:bg-purple-500 hover:text-white w-65">
               <button className="items-center mt-2 ml-3">
                 {" "}
                 <FaBtc size={42} />{" "}
@@ -64,7 +64,7 @@ function AboutSection() {
             happy with the projects carried out.
           </p>
 
-          <button className="px-4 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 transition ease-in-out delay-150 py-2 mt-12 ml-24 text-white bg-purple-600 rounded-lg">
+          <button className="px-4 py-2 mt-12 ml-24 text-white transition duration-300 ease-in-out delay-150 bg-purple-600 rounded-lg hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500">
             Contact Me
           </button>
         </div>
