7efa8ec82830a310e8f6141331091c3b00f48093
amanGit1314 <gitaman8481@gmail.com>
2024-06-14 16:57:06 +0530

    fix: deployment errors missing dependencies

--- src/components/SkillsTile.tsx
+++ src/components/SkillsTile.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 // import { BeakerIcon } from '@heroicons/react/solid'
 
 function SkillsTile({ skill, type, icon }: any) {
   return (
     <div className="items-center mr-10 text-center">
       <div className="flex-1 p-6 text-left bg-purple-400 rounded-full shadow-sm cursor-pointer hover:bg-purple-500 hover:text-white">
-        <img className="w-10 h-10 rounded-full" src={icon} />
+        <Image
+          alt=""
+          height={40}
+          width={40}
+          className="w-10 h-10 rounded-full"
+          src={icon}
+        />
         <p className="w-10 mt-4 text-sm text-center text-gray-100">{type}</p>
       </div>
       <div className="mt-2">{skill}</div>
     </div>
-  )
+  );
 }
 
-export default SkillsTile
+export default SkillsTile;
