4ef1607a1d7f00538a0264a1be3b0db45c7abcde
amanGit1314 <gitaman8481@gmail.com>
2024-09-06 00:44:53 +0530

    dep: globals.css error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800", "900"],
